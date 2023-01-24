8783744e1b6a6e045f44cd9d469d1714a020ce3f
김진영 <skswls0@daum.net>
2023-01-24 20:46:27 +0900

    [feat]: @types/react-beautiful-dnd 설치

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,46 @@
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
 function Main() {
-    return <div>main page!</div>
+    const onDragEnd = () => {
+        console.log('hello')
+    }
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div>
+                <Droppable droppableId="one">
+                    {(provided) => (
+                        <ul
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <Draggable draggableId="first" index={0}>
+                                {(provided) => (
+                                    <li
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                    >
+                                        One
+                                    </li>
+                                )}
+                            </Draggable>
+                            <Draggable draggableId="second" index={1}>
+                                {(provided) => (
+                                    <li
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                    >
+                                        Two
+                                    </li>
+                                )}
+                            </Draggable>
+                        </ul>
+                    )}
+                </Droppable>
+            </div>
+        </DragDropContext>
+    )
 }
 
 export default Main
