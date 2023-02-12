efb17aaeb2d3fd9436a92200cd7f41eee7f49e7e
김진영 <skswls0@daum.net>
2023-02-13 01:07:32 +0900

    feat: Layout 세팅

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
-import { IChildrenProps } from '#types/common'
+import { IChildrenProps } from '#types/common.type'
+
+const Container = styled.div`
+    max-width: 375px;
+    margin: 0 auto;
+    padding: 0 50px;
+    background-color: yellow;
+`
 
 function Layout({ children }: IChildrenProps) {
-    return <>{children}</>
+    return (
+        <>
+            <Container>{children}</Container>
+        </>
+    )
 }
 
 export default Layout
