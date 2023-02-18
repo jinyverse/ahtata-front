7bd84cebeb50f60b36fcfde2ffbf9a8c27c00fa6
김진영 <skswls0@daum.net>
2023-02-18 18:00:54 +0900

    feat: 레이아웃 수정 및 버튼 컴포넌트 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,28 @@
-function Button() {
-    return <button>Button</button>;
-}
+import styled from 'styled-components';
 
-export default Button;
+export const NormalButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 12px 17px 11px 16px;
+    gap: 10px;
+    width: 100%;
+    height: 44px;
+    color: white;
+    background: #ffffff;
+    background-blend-mode: overlay;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(30px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 30px;
+    cursor: pointer;
+
+    background: rgba(255, 255, 255, 0.5);
+    background-blend-mode: overlay;
+    border: 1.5px solid rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(30px);
+    /* Note: backdrop-filter has minimal browser support */
+
+    border-radius: 20px;
+`;
--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,5 +1,34 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100px; // Nav height 컨트롤
+    box-sizing: border-box;
+    bottom: 0;
+    left: 0;
+`;
+
+const Wrapper = styled.div`
+    width: 375px;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.2);
+    background-blend-mode: overlay;
+    box-shadow: 0px -5px 30px rgba(41, 39, 130, 0.1),
+        inset 0px 1px 0.5px rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(40px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 50px 50px 0px 0px;
+`;
+
 function Nav() {
-    return <div>Nav</div>;
+    return (
+        <Container>
+            <Wrapper></Wrapper>
+        </Container>
+    );
 }
 
 export default Nav;
