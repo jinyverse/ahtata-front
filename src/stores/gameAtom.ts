5d99f34fcaacbe09072f2c67fa068bfe776d71e3
김진영 <skswls0@daum.net>
2023-03-05 16:23:06 +0900

    feat: 게임 플레이 관련 기능 구현

--- src/stores/gameAtom.ts
+++ src/stores/gameAtom.ts
@@ -3,7 +3,7 @@ import { ICardData, IPlayStatus } from '@/types/game.type';
 
 // 플레이 중인 유저 화면 데이터
 export const playStatusAtom = atom<IPlayStatus>({
-    key: 'timelineStaus',
+    key: 'playStaus',
     default: {
         artist: null,
         timeline: [],
