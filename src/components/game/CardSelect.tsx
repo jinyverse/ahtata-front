0d0d3916f62eb9e9d788ef51c4f8e51acd0abc36
김진영 <skswls0@daum.net>
2023-04-16 19:44:33 +0900

    아티스트 선택 컴포넌트 구성

--- src/components/game/CardSelect.tsx
+++ src/components/game/CardSelect.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { useSetRecoilState } from 'recoil';
 import { playStatusAtom } from '@/stores/gameAtom';
-import Deck from '@/components/common/Deck';
 
 const Container = styled.div`
     position: absolute;
@@ -10,15 +9,37 @@ const Container = styled.div`
     height: 100%;
     background-color: rgba(1, 1, 1, 0.3);
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     z-index: 999;
+    color: white;
 `;
 
 const Wrapper = styled.div`
-    width: 80%;
-    height: 50%;
-    background-color: white;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.h2`
+    width: 100%;
+    font-size: 18px;
+    color: white;
+`;
+
+const InputSearch = styled.input`
+    width: 100%;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 30px;
+    color: white;
+`;
+
+const CardWrapper = styled.div`
+    display: flex;
+    border: 1px solid white;
+    margin: 10px 0;
+    padding: 5px;
+    cursor: pointer;
 `;
 
 export function CardSelect() {
@@ -30,20 +51,33 @@ export function CardSelect() {
 
     return (
         <Container>
-            <Wrapper>
-                <Deck
-                    width={'8em'}
-                    height={'8em'}
-                    backgroundImage={
-                        'https://image.ajunews.com/content/image/2022/09/12/20220912100327657380.jpg'
-                    }
-                    content={'대충가수이름'}
-                    ranking={'1위'}
-                ></Deck>
-                <button onClick={handleSelectArtist}>아이유 선택하기</button>
-                <button onClick={handleSelectArtist}>임영웅 선택하기</button>
-                <button onClick={handleSelectArtist}>김진영 선택하기</button>
-            </Wrapper>
+            <Title>아티스트 선택하기</Title>
+            <InputSearch placeholder="아티스트 검색" />
+            <div>
+                <span>랭킹순</span>
+                <span>이름순</span>
+            </div>
+            <div>
+                <CardWrapper onClick={handleSelectArtist}>
+                    <span>아티스트 이미지</span>
+                    <p>아티스트 이름</p>
+                    <span>Ranking</span>
+                </CardWrapper>
+                <CardWrapper onClick={handleSelectArtist}>
+                    <span>아티스트 이미지</span>
+                    <p>아티스트 이름</p>
+                    <span>Ranking</span>
+                </CardWrapper>
+                <CardWrapper onClick={handleSelectArtist}>
+                    <span>아티스트 이미지</span>
+                    <p>아티스트 이름</p>
+                    <span>Ranking</span>
+                </CardWrapper>
+            </div>
+            <div>
+                <button>이전</button>
+                <button>선택완료</button>
+            </div>
         </Container>
     );
 }
